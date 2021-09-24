cfe6f7e24d4ac002ecaa7d6a60b640149c4cddf4
chocomilku <andrei.berja078@gmail.com>
2021-09-24 13:57:24 +0800

    fixed commands.js not starting

--- commands.js
+++ commands.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
+require('dotenv').config()
 const clientId = process.env.clientId
 const guildId = process.env.guildId
 const token = process.env.TOKEN
