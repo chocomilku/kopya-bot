b72c88ac7a87fbde29312552513671a20091c29e
chocomilku <andrei.berja078@gmail.com>
2021-09-24 14:12:32 +0800

    added command functions

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 require('dotenv').config()
-const { Client, Intents } = require("discord.js");
+const { Client, Intents, Interaction } = require("discord.js");
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
 client.once("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  const { command } = interaction;
+
+  if (command === "ping") {
+    await interaction.reply('Pong!')
+  } else if (command === "server") {
+    await interaction.reply(`Server Info:\n Server Name: ${interaction.guild.name}\n Total Members: ${interaction.guild.memberCount}`)
+  } else if (command === "user") {
+    await interaction.reply(`Your tag: ${interaction.user.tag}`)
+  }
+
 })
 
 client.login(process.env.TOKEN)
