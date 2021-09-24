94c196a11fccb8242b33507befc4ef072d187e19
chocomilku <andrei.berja078@gmail.com>
2021-09-24 12:51:03 +0800

    remove gitignore files

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 require('dotenv').config()
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.login(process.env.TOKEN)
