1773434942dccec966d4d2fc272d6c335bfba338
chocomilku <andrei.berja078@gmail.com>
2021-09-26 18:49:15 +0800

    made a working prototype of brainlyScraper
    compressed it to a single function

--- test.js
+++ test.js
@@ -1,6 +1,53 @@
 const Brainly = require("brainly-scraper-v2")
 const brain = new Brainly("ph")
 
-brain.search("ph", "How many islands are there in the Philippines?")
-    .then(console.log)
-    .catch(console.error)
+const start = async function(question) {
+    try {
+        function removeTags(str) {
+            if ((str === null) || (str === "")) return false
+            else str = str.toString()
+            return str.replace(/(<([^>]+)>)/ig, '')
+        }
+    
+        function find(query) {
+            return brain.search('ph', query)
+        }
+    
+        if (question === "" || question ===  null || question === undefined) {
+            return console.log("Not valid question")
+        } else {
+            const res = await find(question)
+            for (let i = 0; i < res.length; i++) {
+                let q = `${i}. Question: ${res[i].question.content}`
+                let a = `${i}. Answer: ${res[i].answers[0].content}`
+    
+                console.log(`${removeTags(q)}\n${removeTags(a)}\n`)
+            }
+        }
+        function removeTags(str) {
+            if ((str === null) || (str === "")) return false
+            else str = str.toString()
+            return str.replace(/(<([^>]+)>)/ig, '')
+        }
+    
+        function find(query) {
+            return brain.search('ph', query)
+        }
+    
+        if (question === "" || question ===  null || question === undefined) {
+            return console.log("Not valid question")
+        } else {
+            const res = await find(question)
+            for (let i = 0; i < res.length; i++) {
+                let q = `${i}. Question: ${res[i].question.content}`
+                let a = `${i}. Answer: ${res[i].answers[0].content}`
+    
+                console.log(`${removeTags(q)}\n${removeTags(a)}\n`)
+            }
+        }
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+start("ano ang BTS")
