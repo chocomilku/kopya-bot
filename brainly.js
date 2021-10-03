05ac1d0d89cec3c318ce45c71bfd5ef980b995cc
chocomilku <andrei.berja078@gmail.com>
2021-10-03 13:14:58 +0800

    developing error handling
    tested errors: blank and when scraper doesnt find anything

--- brainly.js
+++ brainly.js
@@ -15,15 +15,19 @@ module.exports = {
                 return brain.search(country, q)
             }
     
-            if (query === "" || query ===  null || query === undefined) {
-                return 'Error'
+            if (query === "" || query === null || query.match(/^ *$/)) {
+                return await Promise.reject(new Error("Not A Valid Question!"));
             } else {
                 const res = await search(query)
-                let data = {"question": res[0].question.content, "questionAuthor": res[0].question.author.username, "subject": res[0].question.education, "grade": res[0].question.grade, "link": `https://brainly.ph/question/${res[0].question.id}`, "answerFormatted": removeTags(res[0].answers[0].content), "answerAuthorId": res[0].answers[0].author.id, "lastActivity": res[0].question.lastActivity}
-                return data
+                if (res[0] == null || res[0] == undefined) {
+                    return await Promise.reject(new Error("An Error Occured. Please try Again"));
+                } else {
+                    let data = {"question": res[0].question.content, "questionAuthor": res[0].question.author.username, "subject": res[0].question.education, "grade": res[0].question.grade, "link": `https://brainly.ph/question/${res[0].question.id}`, "answerFormatted": removeTags(res[0].answers[0].content), "answerAuthorId": res[0].answers[0].author.id, "lastActivity": res[0].question.lastActivity}
+                    return data
+                }
             }
         } catch (err) {
-            console.error(err)
+            return await Promise.reject(new Error(err));
         }
     }
 }
--- tests/test2.js
+++ tests/test2.js
@@ -5,8 +5,14 @@ const find = async (q) => {
 }
 
 const use = async (query) => {
+    
     const res = await find(query)
-    console.log(`Question: ${res.question}\nQuestion By: ${res.questionAuthor}\nSubject: ${res.subject}\nGrade: ${res.grade}\nLink: ${res.link}\nAnswer: ${res.answerFormatted}\n Answer Author ID: ${res.answerAuthorId}\nLast Activity: ${res.lastActivity}`)
+    try {
+        console.log(`Question: ${res.question}\nQuestion By: ${res.questionAuthor}\nSubject: ${res.subject}\nGrade: ${res.grade}\nLink: ${res.link}\nAnswer: ${res.answerFormatted}\n Answer Author ID: ${res.answerAuthorId}\nLast Activity: ${res.lastActivity}`)
+    } catch (err) {
+        console.error(err)
+    }
+   
 }
 
-use('algae')
+use('dhasjdhsajkdhajksdhjkahdwuiqyewueyuowy')
