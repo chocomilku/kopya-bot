720fa2ae18c38233628d6957e95313bc3206615f
chocomilku <andrei.berja078@gmail.com>
2021-10-09 19:10:49 +0800

    used different npm package for pagination
    the other package uses channel.send while this bot uses interaction.reply

--- commands/info.js
+++ commands/info.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
-const { MessageEmbed } = require('discord.js')
-const { pagination } = require('reconlx')
+const { MessageEmbed, MessageButton } = require('discord.js')
+const paginationEmbed = require('discordjs-button-pagination')
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -13,8 +13,42 @@ module.exports = {
             .setAuthor('kopyahan Bot', 'https://cdn.discordapp.com/embed/avatars/0.png', 'https://github.com/chocomilku/kopya-bot')
             .addFields(
                 {name: '/kopya', value: 'Searches your question to brainly.ph and returns the answer'},
-                {name: '/info', value: 'Shows this Embed for information and Changelogs to this Bot'},
+                {name: '/info', value: 'Shows this Embed for all information about this bot'},
             )
-        await interaction.reply({ embeds: [embed]})
+        const embed2 = new MessageEmbed()
+            .setColor('WHITE')
+            .setTitle('Changelog')
+            .setAuthor('kopyahan Bot', 'https://cdn.discordapp.com/embed/avatars/0.png', 'https://github.com/chocomilku/kopya-bot')
+            .addFields(
+                {name: 'v0.8', value: 'added pagination'},
+                {name: 'v0.7', value: 'added embeds to errors\nadded this versioning'},
+                {name: 'v0.6', value: 'added error handling'},
+                {name: 'v0.5', value: 'replies answer to the user'},
+                {name: 'v0.1', value: 'script to fetch answers are working'},
+            )
+
+        const embed3 = new MessageEmbed()
+            .setColor('WHITE')
+            .setTitle('About')
+            .setAuthor('kopyahan Bot', 'https://cdn.discordapp.com/embed/avatars/0.png', 'https://github.com/chocomilku/kopya-bot')
+            .addFields(
+                {name: 'Author', value: 'chocomilku#0437'},
+                {name: 'Github Link', value: 'https://github.com/chocomilku/kopya-bot'},
+            )
+        
+        const button1 = new MessageButton()
+        .setCustomId("previousbtn")
+        .setLabel("Previous")
+        .setStyle("DANGER");
+
+        const button2 = new MessageButton()
+        .setCustomId("nextbtn")
+        .setLabel("Next")
+        .setStyle("SUCCESS");
+            
+        const pages = [embed, embed2, embed3]
+        const buttonList = [button1, button2]
+        const timeout = 120000
+        paginationEmbed(interaction, pages, buttonList, timeout);
     }
 }
