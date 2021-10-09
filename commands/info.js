c89c07bf04a8e3c56dffb87eb88509b494141df2
chocomilku <andrei.berja078@gmail.com>
2021-10-09 17:06:57 +0800

    added npm package reconlx for embed pagination

--- commands/info.js
+++ commands/info.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
 const { MessageEmbed } = require('discord.js')
+const { pagination } = require('reconlx')
 
 module.exports = {
     data: new SlashCommandBuilder()
