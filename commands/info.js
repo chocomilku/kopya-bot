e4c8edcda7b73da9534a7dec83a9808b4b0245ca
chocomilku <andrei.berja078@gmail.com>
2021-10-09 16:54:38 +0800

    updated info.js

--- commands/info.js
+++ commands/info.js
@@ -1,10 +1,19 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
+const { MessageEmbed } = require('discord.js')
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('info')
         .setDescription('All information about this bot.'),
     async execute(interaction) {
-        await interaction.reply('kopyahan Bot')
+        const embed = new MessageEmbed()
+            .setColor('WHITE')
+            .setTitle('Commands')
+            .setAuthor('kopyahan Bot', 'https://cdn.discordapp.com/embed/avatars/0.png', 'https://github.com/chocomilku/kopya-bot')
+            .addFields(
+                {name: '/kopya', value: 'Searches your question to brainly.ph and returns the answer'},
+                {name: '/info', value: 'Shows this Embed for information and Changelogs to this Bot'},
+            )
+        await interaction.reply({ embeds: [embed]})
     }
 }
