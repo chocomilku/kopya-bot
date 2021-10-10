5bb8c2072b47c5c709b463a9a17ce1ccea37c3c4
chocomilku <andrei.berja078@gmail.com>
2021-10-10 19:21:47 +0800

    rewritten function for simplicity

--- brainlyRewrite-feature.js
+++ brainlyRewrite-feature.js
@@ -11,28 +11,29 @@ async function BrainlySearch(query, len = 10) {
          if (query === "" || query === null || query.match(/^ *$/)) {
             throw new Error('Not A Valid Question!')
          } else {
-            function fill(index, question, answer, questionAuthor, questionAuthorId, questionId, subject, grade, answerAuthorId, lastAct, closed) {
-                return {'index': index, 
-                'question': question, 
-                'answer': answer, 
-                'questionAuthor': questionAuthor, 
-                'questionAuthorId': questionAuthorId, 
-                'questionId': questionId, 
-                'subject': subject, 
-                'grade': grade, 
-                'answerAuthorId': answerAuthorId, 
-                'lastActivity': lastAct, 
-                'closed': closed}
-            }
             const res = await search(query, len)
             let data = []
-            for (let index = 0; index < res.length; index++) {
-                const i = res[index]
-                console.log(index)
-                data.push(index)
-                const q = i.question
-                const a = i.answers[0]
-                let append = fill(i, q.content, a.content, q.author.username, q.author.id, q.id, q.education, q.grade, a.author.id, q.lastActivity, q.closed)
+            function fill(index) {
+                const n = res[index]
+                const q = n.question
+                const a = n.answers[0]
+                return {
+                'index': index, 
+                'question': q.content, 
+                'answer': a.content, 
+                'questionAuthor': q.author.username, 
+                'questionAuthorId': q.author.id, 
+                'questionId': q.id, 
+                'subject': q.education, 
+                'grade': q.grade, 
+                'answerAuthorId': a.author.id, 
+                'lastActivity': q.lastActivity, 
+                'closed': q.closed
+                }
+            }
+            for (let i = 0; i < res.length; i++) {
+                console.log(i)
+                let append = fill(i)
                 data.push(append)
             }
             console.log(data)
