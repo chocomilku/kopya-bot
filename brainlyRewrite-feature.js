dbd626a5ba3c28d1b25a02fbd28579f72bca1d3d
chocomilku <andrei.berja078@gmail.com>
2021-10-10 17:37:20 +0800

    brainly feature script is successfully working as intended

--- brainlyRewrite-feature.js
+++ brainlyRewrite-feature.js
@@ -3,7 +3,7 @@ const Brainly = require('brainly-scraper-v2')
 const brain = new Brainly(country)
 const { toMarkdown } = require('./toMarkdown')
 
-async function BrainlySearch(query, len) {
+async function BrainlySearch(query, len = 10) {
     try {
         const convert = (str) => {return toMarkdown(str)}
         const search = (str, length = 10) => {return brain.search(country, str, length)}
@@ -11,8 +11,31 @@ async function BrainlySearch(query, len) {
          if (query === "" || query === null || query.match(/^ *$/)) {
             throw new Error('Not A Valid Question!')
          } else {
+            function fill(index, question, answer, questionAuthor, questionAuthorId, questionId, subject, grade, answerAuthorId, lastAct, closed) {
+                return {'index': index, 
+                'question': question, 
+                'answer': answer, 
+                'questionAuthor': questionAuthor, 
+                'questionAuthorId': questionAuthorId, 
+                'questionId': questionId, 
+                'subject': subject, 
+                'grade': grade, 
+                'answerAuthorId': answerAuthorId, 
+                'lastActivity': lastAct, 
+                'closed': closed}
+            }
             const res = await search(query, len)
-            console.log(res.length)
+            let data = []
+            for (let index = 0; index < res.length; index++) {
+                const i = res[index]
+                console.log(index)
+                data.push(index)
+                const q = i.question
+                const a = i.answers[0]
+                let append = fill(i, q.content, a.content, q.author.username, q.author.id, q.id, q.education, q.grade, a.author.id, q.lastActivity, q.closed)
+                data.push(append)
+            }
+            console.log(data)
          }
     } catch (error) {
         const c = new Error(error)
@@ -22,4 +45,4 @@ async function BrainlySearch(query, len) {
     }
 }
 
-BrainlySearch('rizal')
+BrainlySearch('rizal', 10)
